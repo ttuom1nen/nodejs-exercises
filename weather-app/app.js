4de21229bcd15d461c864fe7698a580dee205f73
ttuom1nen <ttuominen@outlook.com>
2021-01-19 19:44:24 +0200

    Add command line argument for location

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,17 +1,23 @@
 const geocode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
-geocode("Tampere", (error, data) => {
-  if (error) {
-    return console.log(error);
-  }
+const address = process.argv[2];
 
-  forecast(data.latitude, data.longitude, (error, forecastData) => {
+if (!address) {
+  console.log("Please provide an address!");
+} else {
+  geocode(address, (error, data) => {
     if (error) {
       return console.log(error);
     }
 
-    console.log(data.location);
-    console.log(forecastData);
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+      if (error) {
+        return console.log(error);
+      }
+
+      console.log(data.location);
+      console.log(forecastData);
+    });
   });
-});
+}
