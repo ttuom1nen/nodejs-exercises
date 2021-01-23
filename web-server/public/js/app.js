468df72e363bbb1f5d6fbe036be0cfae4673f6c9
ttuom1nen <ttuominen@outlook.com>
2021-01-23 18:04:33 +0200

    Add fetch

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,10 @@
-console.log("Js file loaded");
+(async () => {
+  const resp = await fetch("http://localhost:3000/weather?address=!");
+  const data = await resp.json();
+
+  if (data.error) {
+    console.log(data.error);
+  } else {
+    console.log(data);
+  }
+})();
--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -5,19 +5,22 @@ const geocode = (address, callback) => {
     address
   )}.json?access_token=${process.env.API_KEY_MAPBOX}&limit=1`;
 
-  request({ url, json: true }, (error, { body: { features, message } }) => {
-    if (error) {
-      callback("Unable to connect to location services!", undefined);
-    } else if (!features.length) {
-      callback(message, undefined);
-    } else {
-      callback(undefined, {
-        latitude: features[0].center[1],
-        longitude: features[0].center[0],
-        location: features[0].place_name,
-      });
+  request(
+    { url, json: true },
+    (error, { body: { features, message } } = {}) => {
+      if (error) {
+        callback("Unable to connect to location services!", undefined);
+      } else if (!features.length) {
+        callback(message, undefined);
+      } else {
+        callback(undefined, {
+          latitude: features[0].center[1],
+          longitude: features[0].center[0],
+          location: features[0].place_name,
+        });
+      }
     }
-  });
+  );
 };
 
 module.exports = geocode;
