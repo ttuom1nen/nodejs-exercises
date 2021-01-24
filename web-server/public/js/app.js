4793f56371978a6a1e31cebd3d8cbd209297c4fe
ttuom1nen <ttuominen@outlook.com>
2021-01-24 23:17:40 +0200

    Add address form and data output

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,26 @@
-(async () => {
-  const resp = await fetch("http://localhost:3000/weather?address=!");
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+
+const firstMsg = document.querySelector("#msg-1");
+const secondMsg = document.querySelector("#msg-2");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  fetchWearherData(location);
+});
+
+const fetchWearherData = async (address) => {
+  const resp = await fetch("http://localhost:3000/weather?address=" + address);
   const data = await resp.json();
 
   if (data.error) {
     console.log(data.error);
   } else {
     console.log(data);
+    firstMsg.innerText = data.location;
+    secondMsg.innerText = data.forecast;
   }
-})();
+};
